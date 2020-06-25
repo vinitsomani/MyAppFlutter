9eeb788a71d2ca441bf71f13be03690788523999
vinitsomani <vinitsomani@ymail.com>
2020-06-25 13:32:22 +0530

    row widget design.

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-//*** hello world ***//
-//void main() {
-//  runApp(MaterialApp(
-//    home: Text('hello Bhai'),
-//  )
-//
-//  );
-//}
-
-//*** Simple layout with custom font loading ***//
-//void main() {
-//  runApp(MaterialApp(
-//    home: Scaffold(
-//      appBar: AppBar(
-//        title: Text('My first App'),
-//        centerTitle: true,
-//        backgroundColor: Colors.red[600],
-//      ),
-//      body: Center(
-//        child: Text(
-//          'Hello world',
-//          style: TextStyle(
-//            fontSize: 20.0,
-//            fontWeight: FontWeight.bold,
-//            letterSpacing: 2.0,
-//            color: Colors.grey[600],
-//            fontFamily: 'IndieFlower'
-//          ),
-//        ),
-//      ),
-//      floatingActionButton: FloatingActionButton(
-//        onPressed: () {},
-//        child: Text('click'),
-//        backgroundColor: Colors.red[600],
-//      ),
-//    ),
-//  ));
-//}
-
-
-
 //*** stateless widget for hot reload and reuse widget ***//
 void main() {
   runApp(MaterialApp(
@@ -91,20 +49,23 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-//      body: Container(
-//
-////        padding: EdgeInsets.all(50),
-////        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 100),
-//        padding: EdgeInsets.fromLTRB(10, 20, 30, 40),
-//        margin: EdgeInsets.all(20),
-//        color: Colors.grey[400],
-//        child: Text('hello'),
-//
-//      ),
 
-        body: Padding(
-          padding: EdgeInsets.all(60),
-          child: Text('margin direct to text'),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Text('Hello, world'),
+              FlatButton (
+                onPressed: () {},
+                color: Colors.amber,
+                child: Text('click me'),
+              ),
+              Container(
+                color: Colors.cyan,
+                padding: EdgeInsets.all(30.0),
+                child: Text('Inside Container'),
+              )
+            ],
         ),
 
 
